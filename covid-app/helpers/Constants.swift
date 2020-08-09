727f5f29fbd1a5857a7810a26f9b39e27550ddb0
JasonHaque <zahinjason220434@gmail.com>
2020-08-09 12:41:53 +0600

    code structured

---
+++ covid-app/helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  covid-app
+//
+//  Created by Sanviraj Zahin Haque on 9/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
+
+struct Constants{
+    
+    static let API_KEY = "https://disease.sh/v2/countries/"
+}
--- covid-app/views/CountryTableViewCell.swift
+++ covid-app/views/CountryTableViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CountryTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var CountryNameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
