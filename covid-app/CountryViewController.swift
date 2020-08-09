fdccdaea7675b6f3e8646d5451ce1a042c757bdb
JasonHaque <zahinjason220434@gmail.com>
2020-08-09 12:19:32 +0600

    added new view controllers

---
+++ covid-app/CountryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CountryViewController.swift
+//  covid-app
+//
+//  Created by Sanviraj Zahin Haque on 9/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class CountryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
