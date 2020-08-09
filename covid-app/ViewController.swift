209af3db9dd4a6c289bccc34d798f2b0d2e16f15
JasonHaque <zahinjason220434@gmail.com>
2020-08-09 12:08:13 +0600

    added init code

---
+++ covid-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-app
+//
+//  Created by Sanviraj Zahin Haque on 9/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
