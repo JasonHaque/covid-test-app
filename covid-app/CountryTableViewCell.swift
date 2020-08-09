0c22086dce4b35bf59b9d54dffb46fdbcd7c27a7
JasonHaque <zahinjason220434@gmail.com>
2020-08-09 12:33:57 +0600

    table view cell

---
+++ covid-app/CountryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountryTableViewCell.swift
+//  covid-app
+//
+//  Created by Sanviraj Zahin Haque on 9/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class CountryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
